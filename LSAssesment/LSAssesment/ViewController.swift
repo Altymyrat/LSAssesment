dfaccd4d6c4bbad68f285ed9858f1c7b61b32b5c
TURKCELL\EXT02D18520 <murat.joreyev@consultant.turkcell.com.tr>
2020-06-23 18:54:17 +0300

    Initial commit - project created

---
+++ LSAssesment/LSAssesment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LSAssesment
+//
+//  Created by M.J. on 23.06.2020.
+//  Copyright © 2020 M.J. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
